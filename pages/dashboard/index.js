7421f40412cd2e26774b4766e6e94326ecb091e3
Guilherme Dias <gh.dias1995@gmail.com>
2021-09-19 22:03:17 -0300

    teste de marca d'agua

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,21 +1,10 @@
 import Head from 'next/head'
 import React, { useEffect } from 'react'
-import watermark from 'watermarkjs';
 
 export default function Home() {
   
   useEffect(() => {  
-    var watermarktext = `ESTE TEXTO É UMA MARCA D'ÁGUA!`;
-
-    watermark(['/fb-share.jpg'])
-    .image(watermark.text.lowerRight(watermarktext, '48px Josefin Slab', '#fff', 0.5))
-    .render()
-    .image(watermark.text.upperLeft(watermarktext, '48px Josefin Slab', '#fff', 0.5, 48))
-    .render()
-    .image(watermark.text.center(watermarktext, '48px Josefin Slab', '#fff', 0.5, 48))
-    .then(function (img) {
-      document.getElementById('div1').appendChild(img);    
-    });    
+        
   });
 
   return (
